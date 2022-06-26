b82227139b99e23ae3c15ff316a6641be46c8ee7
fredcarterwolf <fredcarterwolf@gmail.com>
2022-06-26 11:58:35 +0300

    init ticketapp

---
+++ README.md
@@ -0,0 +1,1 @@
+This app building upon Microservice Architecture with TypeScript, Node.js (express.js) and Next.js.
---
+++ auth/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
+
+
