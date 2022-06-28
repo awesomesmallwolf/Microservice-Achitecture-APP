423f89e679dc4826d0e3c29a00502af39beba55a
fredcarterwolf <fredcarterwolf@gmail.com>
2022-06-28 23:53:00 +0300

    authentication completed

---
+++ auth/src/middlewares/require-auth.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+import NotAuthorizedError from '../errors/not-authorized-error';
+
+const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+  if(!req.currentUser)
+  {
+    throw new NotAuthorizedError();
+  }
+
+  next();
+}
+
+export default requireAuth;
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response} from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import validateRequest from '../middlewares/validate-request';
 import { User } from '../models/user';
-import RequestValidationError from '../errors/request-validation-error';
 import BadRequestError from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -14,14 +15,9 @@ router.post('/api/users/signup', [
     .trim()
     .isLength({min: 4, max:20})
     .withMessage('Password must be between 4 and 20 charachters')
-], async (req: Request, res: Response ) => {
-  
-  const errors = validationResult(req);
-
-  if(!errors.isEmpty())
-  {
-    throw new RequestValidationError(errors.array());
-  }
+],
+validateRequest, 
+async (req: Request, res: Response ) => {
 
   const { email, password } = req.body;
 
@@ -33,6 +29,18 @@ router.post('/api/users/signup', [
 
   const user = User.build({ email, password});
   await user.save();
+
+  // Generate JWT
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, process.env.JWT_KEY!);
+
+  // Store it on the session object
+  // req.session.jwt = userJwt; (javascript)
+  req.session = {
+    jwt: userJwt
+  };
 
   res.status(201).send(user);
 
