9d68cc5d1dfd63d3619e0dd32d393eabb97eb792
fredcarterwolf <fredcarterwolf@gmail.com>
2022-06-29 08:22:25 +0300

    index.ts refactored for testing

---
+++ auth/src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import errorHandler from './middlewares/error-handler';
+import NotFoundError from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+})
+
+// if callback mark as async, we must use next function
+// or use express-async-errors npm package
+// app.all('*', async (req, res, next) => {
+//   next(new NotFoundError());
+// })
+
+app.use(errorHandler);
+
+export default app;
