98689c0c68c9df4271821dc6bcae9b253460edea
fredcarterwolf <fredcarterwolf@gmail.com>
2022-07-16 02:19:48 +0300

    order events implemented

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -3,6 +3,8 @@ import { NotAuthorizedError, NotFoundError, requireAuth } from '@tayfurerkenci/c
 import { Order, OrderStatus } from '../models/order';
 import { header } from 'express-validator';
 import mongoose from 'mongoose';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -16,7 +18,7 @@ router.delete('/api/orders/:orderId', requireAuth,
 ], async (req: Request, res: Response) => {
   const { orderId } = req.params;
 
-  const order = await Order.findById(orderId);
+  const order = await Order.findById(orderId).populate('ticket');
 
   if(!order){
     throw new NotFoundError();
@@ -30,7 +32,12 @@ router.delete('/api/orders/:orderId', requireAuth,
   await order.save();
 
   // publishing an event saying this was cancelled!
-  
+  new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id
+    },
+  });
 
   res.status(204).send(order);
 });
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -40,8 +40,8 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-  await mongo.stop();
   await mongoose.connection.close();
+  await mongo.stop();
 });
 
 
