79cde3021250f9aa365cfc5ccc5c3daf357df90f
fredcarterwolf <fredcarterwolf@gmail.com>
2022-07-17 14:26:37 +0300

    optimistic concurrency control implemented

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import { Order, OrderStatus } from './order';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
 
 interface TicketAttrs {
+  id: string;
   title: string;
   price: number;
 }
@@ -9,11 +11,13 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  version: number;
   isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: { id: string, version: number }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema({
@@ -35,9 +39,24 @@ const ticketSchema = new mongoose.Schema({
   }
 });
 
-// Adding method to model
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
+
+// Adding build method to model
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs);
+  return new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price
+  });
+}
+
+// Adding find by event method to model
+ticketSchema.statics.findByEvent = (event: { id: string, version: number}) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1
+  });
 }
 
 // Adding method to document (not model)
